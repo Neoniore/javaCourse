6c836ac834b82e133ebfdb5ee49058d470b6b153
TabulaRasa <lforjust@gmail.com>
2023-10-23 22:50:11 +0300

    Создал класс Employee, создал в нём поля + статическое поле-счётчик.

---
+++ src/pro/sky/my/javaCourse/course1/coursework/Employee.java
@@ -0,0 +1,13 @@
+package pro.sky.my.javaCourse.course1.coursework;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private int department;
+    private double wage;
+
+    public static int counter;
+
+
+}
