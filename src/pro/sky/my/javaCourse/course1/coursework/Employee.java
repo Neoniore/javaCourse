070a55dde98933f1af8a7583dde8e31527e87f7a
TabulaRasa <lforjust@gmail.com>
2023-10-24 14:30:36 +0300

    Добавил поле id в класс Employee; создал геттеры для всех полей класса

--- src/pro/sky/my/javaCourse/course1/coursework/Employee.java
+++ src/pro/sky/my/javaCourse/course1/coursework/Employee.java
@@ -1,13 +1,33 @@
 package pro.sky.my.javaCourse.course1.coursework;
 
 public class Employee {
+    private int id;
     private String name;
     private String surname;
     private String patronymic;
     private int department;
     private double wage;
 
-    public static int counter;
+    private static int counter;
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getPatronymic() {
+        return patronymic;
+    }
+    public int getDepartment() {
+        return department;
+    }
+    public double getWage() {
+        return wage;
+    }
 
 
 }
