5a0249a046282ae35cb09d31f92851a28fa38fba
TabulaRasa <lforjust@gmail.com>
2023-10-24 17:26:40 +0300

    Добавил конструктор; Переопределил метод toString Добавил простую валидацию входных данных департамента и зарплаты и вынес их в отдельные методы

--- src/pro/sky/my/javaCourse/course1/coursework/Employee.java
+++ src/pro/sky/my/javaCourse/course1/coursework/Employee.java
@@ -8,38 +8,76 @@ public class Employee {
     private int department;
     private double wage;
 
+    //счетчик
     private static int counter;
 
+    //геттеры для всех полей класса
     public int getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public String getSurname() {
         return surname;
     }
+
     public String getPatronymic() {
         return patronymic;
     }
+
     public int getDepartment() {
         return department;
     }
+
     public double getWage() {
         return wage;
     }
 
-    public void setDepartment(int department) {
+    //методы для валидации ввода
+    private void validationDepartmentInput(int department) {
         if (department < 1 || department > 5) {
             throw new IllegalArgumentException("Номера департаментов могут быть от 1 до 5");
         }
+    }
+
+    private void validationWageInput(double wage) {
+        if (wage < 0) {
+            throw new IllegalArgumentException("Зарплата не может быть меньше нуля");
+        }
+    }
+
+    //сеттеры департамента и зарплаты
+    public void setDepartment(int department) {
+        validationDepartmentInput(department);
         this.department = department;
     }
 
     public void setWage(double wage) {
-        if (wage < 0) {
-            throw new IllegalArgumentException("Зарплата не может быть меньше нуля");
-        }
+        validationWageInput(wage);
         this.wage = wage;
+    }
+
+    //конструктор
+    public Employee(String name, String surname, String patronymic, int department, double wage) {
+        validationDepartmentInput(department);
+        validationWageInput(wage);
+
+        counter++;
+
+        id = counter;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.wage = wage;
+    }
+
+    //переопределение методов
+    @Override
+    public String toString() {
+        return "ФИО: " + name + " " + " " + patronymic + " " + " " + surname + "\n" + "Отдел " + department + "\nЗарплата " + wage + "\nid " + id;
     }
 }
--- src/pro/sky/my/javaCourse/course1/coursework/Tests.java
+++ src/pro/sky/my/javaCourse/course1/coursework/Tests.java
@@ -2,6 +2,8 @@ package pro.sky.my.javaCourse.course1.coursework;
 
 public class Tests {
     public static void main(String[] args) {
-        System.out.println("My coursework");
+        Employee firtEmployee = new Employee("Александр", "Панченко", "Витальевич", 5, 1_500_000);
+
+        System.out.println(firtEmployee);
     }
 }
