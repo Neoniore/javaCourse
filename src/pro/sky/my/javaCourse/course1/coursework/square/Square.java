eebafce02518a6c058672a45fb88e96f8ce1e8b1
TabulaRasa <lforjust@gmail.com>
2023-10-24 23:35:52 +0300

    Решение задачи из 6 билета

---
+++ src/pro/sky/my/javaCourse/course1/coursework/square/Square.java
@@ -0,0 +1,12 @@
+package pro.sky.my.javaCourse.course1.coursework.square;
+
+public class Square {
+    public static void main(String[] args) {
+        double num1 = -10;
+        findSquare(num1);
+    }
+
+    public static void findSquare(double number) {
+        System.out.println("number * number = " + number * number);
+    }
+}
