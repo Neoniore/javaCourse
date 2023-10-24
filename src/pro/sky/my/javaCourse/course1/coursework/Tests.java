648ece02f4d8b8c649811f48faee67d2aad93259
TabulaRasa <lforjust@gmail.com>
2023-10-24 19:48:28 +0300

    Создал методы поиска сотрудников с минимальной и максимальной зарплатами

--- src/pro/sky/my/javaCourse/course1/coursework/Tests.java
+++ src/pro/sky/my/javaCourse/course1/coursework/Tests.java
@@ -16,26 +16,54 @@ public class Tests {
         staff[8] = new Employee("Елисей", "Иванов", "Львович", 5, 145000);
         staff[9] = new Employee("Андрей", "Петровский", "Степанович", 5, 250000);
 
-        //showAllEmployees(staff);
-        System.out.printf("Сумма затрат на зарплаты в месяц %.2fр", calculateTheAmountOfSalaryCosts(staff));
+        showAllEmployees(staff);
+        System.out.printf("------\nСумма затрат на зарплаты в месяц\n------\n%.2fр\n\n\n", calculateTheAmountOfSalaryCosts(staff));
+        System.out.printf("------\nCотрудник с минимальной зарплатой\n------\n%s\n\n\n", searchingEmployeeWithMinSalary(staff));
+        System.out.printf("------\nCотрудник с максимальной зарплатой\n------\n%s\n\n\n", searchingEmployeeWithMaxSalary(staff));
     }
 
     //Метод вывода информации о всех сотрудниках
     public static void showAllEmployees(Employee[] staff) {
-        System.out.println("Все сотрудники");
+        System.out.println("------\nВсе сотрудники\n------");
         for (Employee employee : staff) {
             System.out.println(employee + "\n");
         }
+        System.out.println();
     }
 
     //Метод рассчета и вывода суммы затрат на зарплаты в месяц
     public static double calculateTheAmountOfSalaryCosts(Employee[] staff) {
         double ammount = 0;
-        for (int i = 0; i < staff.length; i++) {
-            ammount += staff[i].getWage();
+        for (Employee employee : staff) {
+            ammount += employee.getWage();
         }
         return ammount;
     }
 
+    //Метод поиска сотрудника с минимальной зарплатой
+    public static Employee searchingEmployeeWithMinSalary(Employee[] staff) {
+        double minWage= 10_000_000_000d;
+        Employee employeeWithMinSalary = staff[0];
+        for (Employee employee : staff) {
+            if (employee.getWage() < minWage) {
+                minWage = employee.getWage();
+                employeeWithMinSalary = employee;
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    //Метод поиска сотрудника с максимальной зарплатой
+    public static Employee searchingEmployeeWithMaxSalary(Employee[] staff) {
+        double maxWage= -1d;
+        Employee employeeWithMaxSalary = staff[0];
+        for (Employee employee : staff) {
+            if (employee.getWage() > maxWage) {
+                maxWage = employee.getWage();
+                employeeWithMaxSalary = employee;
+            }
+        }
+        return employeeWithMaxSalary;
+    }
 
 }
