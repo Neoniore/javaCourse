0af08efebc6f551f706d492c6bb19365ad0cd65b
TabulaRasa <lforjust@gmail.com>
2023-11-19 19:23:16 +0300

    написал класс Hogwarts

--- src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Hogwarts.java
+++ src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Hogwarts.java
@@ -4,5 +4,24 @@ public class Hogwarts {
     private int powerOfMagic;
     private int transgressionDistance;
 
+    public Hogwarts(int powerOfMagic, int transgressionDistance) {
+        this.powerOfMagic = powerOfMagic;
+        this.transgressionDistance = transgressionDistance;
+    }
 
+    public int getPowerOfMagic() {
+        return powerOfMagic;
+    }
+
+    public void setPowerOfMagic(int powerOfMagic) {
+        this.powerOfMagic = powerOfMagic;
+    }
+
+    public int getTransgressionDistance() {
+        return transgressionDistance;
+    }
+
+    public void setTransgressionDistance(int transgressionDistance) {
+        this.transgressionDistance = transgressionDistance;
+    }
 }
