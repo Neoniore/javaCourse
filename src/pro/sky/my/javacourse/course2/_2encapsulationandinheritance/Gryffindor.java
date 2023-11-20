79b24a72053929577ee93a48bc9f058c4b8a8f57
TabulaRasa <lforjust@gmail.com>
2023-11-20 13:08:03 +0300

    описал класс Gryffindor

--- src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Gryffindor.java
+++ src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Gryffindor.java
@@ -1,5 +1,26 @@
 package pro.sky.my.javacourse.course2._2encapsulationandinheritance;
 
-public class Gryffindor {
+public class Gryffindor extends Hogwarts {
+    int nobility;
+    int bravery;
 
+    public Gryffindor(int powerOfMagic, int transgressionDistance, int nobility, int bravery) {
+        super(powerOfMagic, transgressionDistance);
+    }
+
+    public int getNobility() {
+        return nobility;
+    }
+
+    public void setNobility(int nobility) {
+        this.nobility = nobility;
+    }
+
+    public int getBravery() {
+        return bravery;
+    }
+
+    public void setBravery(int bravery) {
+        this.bravery = bravery;
+    }
 }
