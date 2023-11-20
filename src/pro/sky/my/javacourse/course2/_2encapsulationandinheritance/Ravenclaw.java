862bf61d5f9f8d545540941eb3f898a0c34438f8
TabulaRasa <lforjust@gmail.com>
2023-11-20 13:32:51 +0300

    описал класс Ravenclaw

--- src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Ravenclaw.java
+++ src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Ravenclaw.java
@@ -1,6 +1,48 @@
 package pro.sky.my.javacourse.course2._2encapsulationandinheritance;
 
-public class Ravenclaw {
-    // Когтевран
+// Когтевран
 
+public class Ravenclaw extends Hogwarts {
+
+    int mind;
+    int wisdom;
+    int creativity;
+
+    /**
+     *
+     * @param mind - ум
+     * @param wisdom - мудрость
+     * @param creativity - творчество
+     */
+
+    public Ravenclaw(int powerOfMagic, int transgressionDistance, int mind, int wisdom, int creativity) {
+        super(powerOfMagic, transgressionDistance);
+        this.mind = mind;
+        this.wisdom = wisdom;
+        this.creativity = creativity;
+    }
+
+    public int getMind() {
+        return mind;
+    }
+
+    public void setMind(int mind) {
+        this.mind = mind;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getCreativity() {
+        return creativity;
+    }
+
+    public void setCreativity(int creativity) {
+        this.creativity = creativity;
+    }
 }
