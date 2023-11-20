251ef5d3f7c29131bf1cbec839c20825c02ca5cb
TabulaRasa <lforjust@gmail.com>
2023-11-20 13:05:26 +0300

    описал класс Slytherin

--- src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Slytherin.java
+++ src/pro/sky/my/javacourse/course2/_2encapsulationandinheritance/Slytherin.java
@@ -1,5 +1,53 @@
 package pro.sky.my.javacourse.course2._2encapsulationandinheritance;
 
-public class Slytherin {
+public class Slytherin extends Hogwarts {
+    private int cunning;
+    private int determination;
+    private int ambition;
+    private int resourcefulness;
+    private int thirstForPower;
 
+    public Slytherin(int powerOfMagic, int transgressionDistance, int cunning, int determination, int ambition, int resourcefulness, int thirstForPower) {
+        super(powerOfMagic, transgressionDistance);
+    }
+
+    public int getCunning() {
+        return cunning;
+    }
+
+    public void setCunning(int cunning) {
+        this.cunning = cunning;
+    }
+
+    public int getDetermination() {
+        return determination;
+    }
+
+    public void setDetermination(int determination) {
+        this.determination = determination;
+    }
+
+    public int getAmbition() {
+        return ambition;
+    }
+
+    public void setAmbition(int ambition) {
+        this.ambition = ambition;
+    }
+
+    public int getResourcefulness() {
+        return resourcefulness;
+    }
+
+    public void setResourcefulness(int resourcefulness) {
+        this.resourcefulness = resourcefulness;
+    }
+
+    public int getThirstForPower() {
+        return thirstForPower;
+    }
+
+    public void setThirstForPower(int thirstForPower) {
+        this.thirstForPower = thirstForPower;
+    }
 }
