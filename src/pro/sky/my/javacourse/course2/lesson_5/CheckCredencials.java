9bc4590b3a777a7300c6b49b0be761e9005cd82c
TabulaRasa <lforjust@gmail.com>
2023-12-25 03:08:32 +0300

    Создал кастомные исключения для логина и пароля; Создал класс Т.к. требования по содержанию символов для логина и пароля одинаковы, в классе CheckCredencials методы checkLogin и checkPassword заменил на один - checkCredencialsString

--- src/pro/sky/my/javacourse/course2/lesson_5/CheckCredencials.java
+++ src/pro/sky/my/javacourse/course2/lesson_5/CheckCredencials.java
@@ -2,24 +2,16 @@ package pro.sky.my.javacourse.course2.lesson_5;
 
 public class CheckCredencials {
     public static void checkLoginPassword(String login, String password, String confirmPassword) {
-        if (checkLogin(login) != 0){
+        if (checkCredencialsString(login) != 0){
             throw new WrongLoginException();
         }
-        if (!password.equals(confirmPassword) && checkPassword(password) != 0 && checkPassword(confirmPassword) != 0) {
+        if (!password.equals(confirmPassword) && checkCredencialsString(password) != 0 && checkCredencialsString(confirmPassword) != 0) {
             throw new WrongPasswordException();
         }
     }
 
-    private static int checkLogin(String login) {
-        if (login.contains("dsad") && !login.isEmpty() && login.length() <= 20) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    private static int checkPassword(String password) {
-        if (password.contains("dasd") && !password.isEmpty() && password.length() <= 20) {
+    private static int checkCredencialsString(String s) {
+        if (s.contains("dsad") && !s.isEmpty() && s.length() <= 20) {
             return 0;
         } else {
             return 1;
