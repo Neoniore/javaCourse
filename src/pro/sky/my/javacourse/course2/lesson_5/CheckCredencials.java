3e6b5d4be9ecb3bf6c723b045f2b12648134c10c
TabulaRasa <lforjust@gmail.com>
2023-12-25 04:15:34 +0300

    Исправил логику в if в проверке паролей; Добавил проверку входящих в строку символов с помощью метода matches и регулярного выражения;
    Готовая домашка

--- src/pro/sky/my/javacourse/course2/lesson_5/CheckCredencials.java
+++ src/pro/sky/my/javacourse/course2/lesson_5/CheckCredencials.java
@@ -6,13 +6,16 @@ public class CheckCredencials {
         if (checkCredencialsString(login) != 0){
             throw new WrongLoginException();
         }
-        if (!password.equals(confirmPassword) && checkCredencialsString(password) != 0 && checkCredencialsString(confirmPassword) != 0) {
+        if (!password.equals(confirmPassword)
+                || checkCredencialsString(password) != 0
+                || checkCredencialsString(confirmPassword) != 0)
+        {
             throw new WrongPasswordException();
         }
     }
 
     private static int checkCredencialsString(String s) {
-        if (s != null && !s.isEmpty() && s.length() <= 20 && s.contains("")) {
+        if (s != null && !s.isEmpty() && s.length() <= 20 && s.matches("[a-zA-Z-0-9_]*")) {
             return 0;
         } else {
             return 1;
--- src/pro/sky/my/javacourse/course2/lesson_5/Tests.java
+++ src/pro/sky/my/javacourse/course2/lesson_5/Tests.java
@@ -3,13 +3,13 @@ package pro.sky.my.javacourse.course2.lesson_5;
 public class Tests {
     public static void main(String[] args) {
         String login = "log_in";
-        String password = "D_1hWiKjjP_9";
-        String confirmPassword = "D_1hWiKjjP_9";
+        String password = "*D_1hWiKjjP_9";
+        String confirmPassword = "*D_1hWiKjjP_9";
 
         try {
             CheckCredencials.checkLoginPassword(login, password, confirmPassword);
         } catch (WrongLoginException e) {
-            System.out.println("Логин не соответствуют требованиям");
+            System.out.println("Логин не соответствует требованиям");
         } catch (WrongPasswordException e) {
             System.out.println("Пароль не соответствуют требованиям или пароли не совпадают");
         }
